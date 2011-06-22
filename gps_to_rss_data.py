7a4f5565f3455a87a28e2ee17ba386097c4af5de
Thorbjörn 'Puggan' Backlund <from_git@puggan.se>
2011-06-22 23:16:14 +0200

    added function to save as rss

--- gps_to_rss_data.py
+++ gps_to_rss_data.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+import datetime
 import gps
 import math
-import random
 import sys
 
 files = set()
@@ -40,9 +40,22 @@ if "mindiff" in keywords:
 	min_diff = 360 * float(keywords["mindiff"]) / 40000000
 else:
 	min_diff = 0.00045
-	
+
+if "template" in keywords:
+	template = keywords["template"]
+else:
+	template = "georss_template.xml"
+
+if "outfile" in keywords:
+	outfile = keywords["outfile"]
+	template_data = open(template, 'r').read()
+else:
+	outfile = "";
+
 last_lat = 0.00
 last_lon = 0.00
+
+positions = ""
 
 session = gps.gps(**gps_server)
 
@@ -54,9 +67,14 @@ for report in session:
 		diff_lon = report.lon - last_lon
 		diff = diff_lat * diff_lat + diff_lon * diff_lon * math.fabs(math.cos(report.lat))
 		if diff >= min_diff:
-			print "%(lat)s %(lon)s " % report
+			new_pos = "%(lat)s %(lon)s " % report
 			last_lat = report.lat
 			last_lon = report.lon
+			print new_pos
+			positions += new_pos
+			if outfile:
+				georss = template_data % {"position": positions, "timestamp": datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S") + "Z"}
+				open(outfile, 'w').write(georss)
 	except AttributeError:
 		pass
 	except KeyError:
